946d93703971791ca75733cd79d40db1f742126e
Alex2 <aleksander1802@gmail.com>
2023-03-15 16:44:10 +0300

    feat: added global.css, added 404 page, added htag component

---
+++ react-components/src/models/cards.ts
@@ -0,0 +1,16 @@
+export interface ICards {
+  _id: string;
+  index: number;
+  isActive: boolean;
+  balance: string;
+  url: string;
+  age: number;
+  eyeColor: string;
+  name: string;
+  gender: string;
+  company: string;
+  email: string;
+  phone: string;
+  address: string;
+  registered: string;
+}
---
+++ react-components/src/pages/404.tsx
@@ -0,0 +1,9 @@
+import { Htag } from '../components/Htag/Htag';
+
+export function Error404(): JSX.Element {
+  return (
+    <>
+      <Htag tag="h1">Ошибка 404</Htag>
+    </>
+  );
+}
