dcce1e35bf3a9b3c91abbd52c2ad0da96bc28f5c
Alex2 <aleksander1802@gmail.com>
2023-03-19 00:11:28 +0300

    feat: added github svg into footer

--- react-components/src/layout/Footer/Footer.tsx
+++ react-components/src/layout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { FooterProps } from './Footer.props';
+import GitIcon from './github.svg';
 
 import cn from 'classnames';
 
@@ -8,6 +9,9 @@ export const Footer = ({ className }: FooterProps): JSX.Element => {
   return (
     <footer className={cn(className, styles.footer)}>
       <h3>React Components © 2023 Designed by Aleksander Rudenko</h3>
+      <a href="https://github.com/aleksander1802" target="_blank" rel="noreferrer">
+        <img width={40} height={40} src={GitIcon} alt="Github icon" />
+      </a>
     </footer>
   );
 };
