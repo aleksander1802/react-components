484abc95f13b0743592b89dd63c3afe72d81041f
Alex2 <aleksander1802@gmail.com>
2023-03-22 15:08:11 +0300

    feat: added forwardRef for Input component

--- react-components/src/components/Input/Input.tsx
+++ react-components/src/components/Input/Input.tsx
@@ -1,12 +1,16 @@
 import { InputProps } from './Input.props';
+
 import cn from 'classnames';
 
 import styles from './Input.module.css';
+import { ForwardedRef, forwardRef } from 'react';
 
-export const Input = ({ className, ...props }: InputProps): JSX.Element => {
-  return (
-    <div className={cn(className, styles.inputWrapper)}>
-      <input className={cn(styles.input)} {...props} />
-    </div>
-  );
-};
+export const Input = forwardRef(
+  ({ className, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
+    return (
+      <div className={cn(className, styles.inputWrapper)}>
+        <input className={cn(styles.input)} ref={ref} {...props} />
+      </div>
+    );
+  }
+);
