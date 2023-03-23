8ec9d9e10f4372e461a0b4cb1b4eb37cb67da607
Alex2 <aleksander1802@gmail.com>
2023-03-23 20:11:26 +0300

    feat: added all required fileds to form

--- react-components/src/components/AddCardForm/AddCardForm.tsx
+++ react-components/src/components/AddCardForm/AddCardForm.tsx
@@ -2,81 +2,149 @@ import { FormProps } from './AddCardForm.props';
 import { Input } from '../../components/Input/Input';
 import { Button } from '../../components/Button/Button';
 import React, { Component, FormEvent, createRef } from 'react';
+import { Htag } from '../../components/Htag/Htag';
 
 import cn from 'classnames';
 
 import styles from './AddCardForm.module.css';
 
 export class AddCardForm extends Component<FormProps> {
-  constructor(props: FormProps) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  selectSexOptions: string[] = ['Male', 'Female', 'Optimus Prime'];
+  options = [
+    { id: 1, name: 'Male' },
+    { id: 2, name: 'Female' },
+    { id: 3, name: 'Optimus Prime' },
+  ];
+
+  eye = ['blue', 'brown', 'green'];
+  messengers = ['Telegram', 'Viber', 'WhatsApp', 'Skype', 'VK Messenger'];
+  genderRefs: HTMLInputElement[] = [];
+  messengersRefs: HTMLInputElement[] = [];
+
+  setRef = (ref: HTMLInputElement) => {
+    this.genderRefs.push(ref);
+  };
+
+  setMessangersRef = (ref: HTMLInputElement) => {
+    this.messengersRefs.push(ref);
+  };
 
   textInputRef = createRef<HTMLInputElement>();
+  dateRef = createRef<HTMLInputElement>();
+  selectEyeColorRef = createRef<HTMLSelectElement>();
+  ageInputRef = createRef<HTMLInputElement>();
+  genderRef = createRef<HTMLInputElement>();
+  messengersInputRef = createRef<HTMLInputElement>();
+  resetForm = createRef<HTMLFormElement>();
 
-  handleClick() {
-    console.log(this.textInputRef.current?.value);
-  }
+  handleClick = (): void => {
+    this.genderRefs.find((item) => item.checked);
+    this.messengersRefs.map((item) => console.log(item.checked === true ? item.value : null));
+  };
 
-  handleSubmit(e: FormEvent) {
+  handleSubmit = (e: FormEvent): void => {
     e.preventDefault();
-  }
+  };
 
   render() {
     const { className } = this.props;
     return (
-      <div className={cn(className, styles.formWrapper)}>
+      <div className={cn(className, styles.container)}>
         <form onSubmit={(e) => this.handleSubmit(e)} className={cn(styles.form)} {...this.props}>
-          Create new card form:
-          <label htmlFor="name">
-            First name:
-            <Input
-              required
-              id="name"
-              type={'text'}
-              placeholder={'Enter name'}
-              ref={this.textInputRef}
-            />
-          </label>
-          <label htmlFor="lastName">
-            Last Name:
-            <Input required id="lastName" type={'text'} placeholder={'Enter last name'} />
-          </label>
-          <label htmlFor="age">
-            Age:
-            <Input
-              required
-              id="age"
-              type={'number'}
-              placeholder={'Enter age'}
-              className={styles.age}
-            />
-          </label>
-          <label htmlFor="sex">
-            Sex:
-            <select id="sex">
-              {this.selectSexOptions.map((item) => {
-                return (
-                  <option key={item} value={item}>
-                    {item}
-                  </option>
-                );
-              })}
-            </select>
-          </label>
-          <Input name="exampleInput" type={'test'} />
-          <Input type={'radio'} />
-          <Input type={'file'} />
-          <Button
-            type="submit"
-            appearance="primary"
-            className={styles.button}
-            onClick={this.handleClick}
-          >
-            Submit
-          </Button>
+          <Htag tag="h2">Creating a new card form:</Htag>
+          <div className={styles.wrapper}>
+            <label htmlFor="name" className={styles.item}>
+              <span>Name:</span>
+              <Input
+                required
+                id="name"
+                type={'text'}
+                placeholder={'Enter name'}
+                ref={this.textInputRef}
+              />
+            </label>
+
+            <label htmlFor="date" className={styles.item}>
+              <span>Birthday:</span>
+              <Input required id="date" type={'date'} className={styles.date} ref={this.dateRef} />
+            </label>
+
+            <label htmlFor="eye" className={styles.item}>
+              <span>Eye color:</span>
+              <select id="eye" className={styles.select} ref={this.selectEyeColorRef}>
+                {this.eye.map((item) => {
+                  return (
+                    <option value={item} key={item}>
+                      {item}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+
+            <label htmlFor="age" className={styles.item}>
+              <span>Age:</span>
+              <Input
+                required
+                id="age"
+                type={'number'}
+                placeholder={'Enter age'}
+                className={styles.age}
+                ref={this.ageInputRef}
+              />
+            </label>
+
+            <div className={styles.options}>
+              <span>Messengers: </span>
+              <div className={styles.sex}>
+                {this.messengers.map((item) => {
+                  return (
+                    <label key={item} className={styles.option}>
+                      <span>{item}</span>
+                      <Input
+                        id="check"
+                        type="checkbox"
+                        name="checkbox"
+                        ref={this.setMessangersRef}
+                        value={item}
+                      />
+                    </label>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className={styles.options}>
+              <span>Gender: </span>
+              <div className={styles.sex}>
+                {this.options.map((item) => {
+                  return (
+                    <label key={item.id} className={styles.option}>
+                      <span>{item.name}</span>
+                      <Input
+                        required
+                        id="id"
+                        type="radio"
+                        name="gender"
+                        ref={this.setRef}
+                        value={item.id}
+                      />
+                    </label>
+                  );
+                })}
+              </div>
+            </div>
+
+            <Input type={'file'} />
+
+            <Button
+              type="submit"
+              appearance="primary"
+              className={styles.button}
+              onClick={this.handleClick}
+            >
+              <span>Submit</span>
+            </Button>
+          </div>
         </form>
       </div>
     );
--- react-components/src/components/Htag/Htag.props.ts
+++ react-components/src/components/Htag/Htag.props.ts
@@ -1,3 +1,6 @@
+import { ReactNode } from 'react';
+
 export interface HtagProps {
   tag: 'h1' | 'h2' | 'h3' | undefined;
+  children: ReactNode;
 }
