ef712676bc1c93fb80687be8e6c8ed8b443c67e7
Alex2 <aleksander1802@gmail.com>
2023-04-08 01:48:28 +0300

    fix: fixed tests for MainPage.test

--- react-components/src/pages/Main/MainPage.test.tsx
+++ react-components/src/pages/Main/MainPage.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import { Cards } from '../../components/Cards/Cards';
 import { Search } from '../../components/Search/Search';
@@ -6,12 +6,12 @@ import MainPage from './MainPage';
 
 describe('should render Cards and Search components', () => {
   test('<Cards /> component rendered', () => {
-    const cards = render(<Cards />);
+    const cards = render(<Cards query={''} />);
     expect(cards).toBeTruthy();
     expect(cards).toBeDefined();
   });
   test('<Search /> component rendered', () => {
-    const search = render(<Search />);
+    const search = render(<Search query="d" search={vi.fn()} />);
     expect(search).toBeTruthy();
     expect(search).toBeDefined();
   });
