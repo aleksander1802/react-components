9bd218b46f89a2b40592e1878dcf1cb3d148db72
Alex2 <aleksander1802@gmail.com>
2023-04-21 13:31:12 +0300

    refactor: removed useless comments

--- react-components/cypress.config.ts
+++ react-components/cypress.config.ts
@@ -1,30 +1,3 @@
-// import { defineConfig } from 'cypress';
-// import task from '@cypress/code-coverage/task';
-
-// export default defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // eslint-disable-next-line @typescript-eslint/no-var-requires
-//       task(on, config);
-
-//       return config;
-//     },
-//   },
-// });
-// import { defineConfig } from 'cypress';
-// import coverage from '@cypress/code-coverage/task';
-
-// export default defineConfig({
-//   e2e: {
-//     baseUrl: 'http://localhost:5173/',
-//     setupNodeEvents(on, config) {
-//       // eslint-disable-next-line @typescript-eslint/no-var-requires
-//       require('@cypress/code-coverage/task')(on, config);
-
-//       return config;
-//     },
-//   },
-// });
 import { defineConfig } from 'cypress';
 import coverage from '@cypress/code-coverage/task';
 
